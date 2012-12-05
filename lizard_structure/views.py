024b7bc233aabea211a30bce4e657b1f4f6b2b9c
Reinout van Rees <reinout@vanrees.org>
2012-12-05 02:51:57 +0100

    cleaned out lizard-ui and lizard-map

--- lizard_structure/views.py
+++ lizard_structure/views.py
@@ -3,19 +3,5 @@ from __future__ import unicode_literals
 
 from django.utils.translation import ugettext as _
 # from django.core.urlresolvers import reverse
-# from lizard_map.views import MapView
-# from lizard_ui.views import UiView
 
 # from lizard_structure import models
-
-
-# class TodoView(UiView):
-#     """Simple view without a map."""
-#     template_name = 'lizard_structure/todo.html'
-#     page_title = _('TODO view')
-
-
-# class Todo2View(MapView):
-#     """Simple view with a map."""
-#     template_name = 'lizard_structure/todo2.html'
-#     page_title = _('TODO 2 view')
