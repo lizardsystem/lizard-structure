1dfbd2e99cc76a7d27ab57be66d407feea48999c
Reinout van Rees <reinout@vanrees.org>
2012-12-18 21:42:02 +0100

    Making sure a base class's docstring is used if we don't have one.
    This way our API documentation of the base classes is re-used by the classes
    that actually implement them in all the various Lizard apps.
    
    Also, the docstring is now rendered with restructured text instead of markdown.

--- lizard_structure/views.py
+++ lizard_structure/views.py
@@ -19,19 +19,71 @@ But: a project also has information on itself, as has a project, etc.
 
 """
 from __future__ import unicode_literals
+import inspect
 
 # from django.core.urlresolvers import reverse
 # from django.utils.translation import ugettext as _
-from rest_framework.response import Response
+from django.contrib.admindocs.utils import trim_docstring
+from django.utils.safestring import mark_safe
 from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+import docutils
 import pkginfo
 
 
-class DataSourceView(GenericAPIView):
+class BaseAPIView(GenericAPIView):
+
+    def get_description(self, html=False):
+        """Return the view's docstring as a description.
+
+        This method is a customization of Django REST framework's. There are
+        two changes:
+
+        - Our parent's docstring is used if we don't have one ourselves. This
+          makes it easy to use a base class with proper documentation on which
+          items to expect. The documentation is propagated to every API that
+          uses the base class.
+
+        - The docstring is parsed with restructuredtext syntax instead of
+          markdown (markdown is preferred by Django REST framework).
+
+        """
+        description = self.__doc__
+        if description is None:
+            # Trick to get our parent's docstring as a fallback if we don't
+            # have one ourselves. From
+            # http://stackoverflow.com/a/13937525/27401 .
+            try:
+                description = next(
+                    cls.__doc__ for cls in inspect.getmro(type(self))
+                    if cls.__doc__ is not None)
+            except StopIteration:
+                pass
+
+        description = trim_docstring(description)
+        if html:
+            parts = docutils.core.publish_parts(description,
+                                                writer_name='html')
+            return mark_safe(parts['fragment'])
+        return description
+
+
+class DataSourceView(BaseAPIView):
     """
     Information about the data source itself and its list of projects.
 
-    Use this to discover the projects you can show in your user interface.
+    Use this to discover the projects you can show in your user interface. The
+    result is a dictionary with the following items:
+
+    about_ourselves
+        Metadata about ourselves, like the software version that generated
+        it. Do not depend on the actual items in here, just display them when
+        desired as background information.
+
+    projects
+        The list of available projects. A project is a collection of
+        layers you can show on a map or in an overview.
+
     """
 
     @property
